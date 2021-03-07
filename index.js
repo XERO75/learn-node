96be873de0b75ac18d7325a9b7850ed1b5df09ff
XERO75 <xerolry@gmail.com>
2021-03-07 23:21:16 +0800

    ud

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(cors())
 
 var morgan = require('morgan');
 app.use(express.json());
-
+app.use(express.static('build'))
 app.use(
   morgan(function (tokens, req, res) {
     console.log(req.body);
