101955e86bc8a93d0e9fece062ebc487f869bc77
XERO75 <xerolry@gmail.com>
2021-03-22 00:56:35 +0800

    update

--- models/note.js
+++ models/note.js
@@ -1,33 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
-console.log('connecting to', url);
-
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log('Connected to the Database. Yayzow!');
-  })
-  .catch((err) => {
-    console.log('error connecting to MongoDB:', err.message);
-  });
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: { 
+    type: Date,
+    required: true
+  },
   important: Boolean,
-});
-// const Note = mongoose.model('Note', noteSchema);
+})
+
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
