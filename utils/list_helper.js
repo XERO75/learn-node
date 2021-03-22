a8a538f7b0a0c8141bfb8a51e3f8fc348f88097e
XERO75 <xerolry@gmail.com>
2021-03-22 23:18:23 +0800

    ud

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,31 @@ const totalLikes = (blogs) => {
   },0)
 }
 
+const favoriteBlog = (blogs) => {
+  let max = blogs.sort((a,b) => {
+    return b.likes - a.likes
+  })
+  return max[0]
+}
+
+const mostBlogs = blogs => {
+  const arr = []
+  blogs.forEach((item, idx) => {
+    let target = arr.find(i => i.author === item.author)
+    target ? target.count += 1 : arr.push({...item, count: 1})
+  })
+  const sorted = arr.sort((a,b) => {
+    return b.count - a.count
+  })
+  return {
+    author: sorted[0].author,
+    blogs: sorted[0].count,
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
