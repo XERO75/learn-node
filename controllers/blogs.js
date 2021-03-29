b2416226b40f228c556be00f6607c0a0dca3c50e
xavier <laishaoliang@sina.cn>
2021-03-29 18:32:41 +0800

    ud

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,47 +1,60 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
- 
-blogsRouter.get('/', (req, res) => {
-  Blog.find({})
-    .then(notes => {
-      res.json(notes)
-    })
-})
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-blogsRouter.post('/', (req, res, next) => {
-  const body = req.body
+const getTokenFrom = (req) => {
+  const authorization = req.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
+blogsRouter.get('/', (req, res) => {
+  Blog.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
+
+blogsRouter.post('/', async (req, res, next) => {
+  const body = req.body;
+  const token = getTokenFrom(req);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
-  })
+    likes: body.likes,
+    user: user._id,
+  });
 
-  blog.save()
-    .then(savedBlog =>{
-      res.json(savedBlog)
-    })
-    .catch(err => next(err))
-})
+  const savedBlog = await blog.save();
+  user.blogs = user.notes.concat(savedBlog._id);
+  res.json(savedBlog);
+});
 
-blogsRouter.delete('/:id',  (req, res, next) => {
-   Blog.findByIdAndRemove(req.params.id).then(() => {
-     res.status(204).end()
-   })
-})
+blogsRouter.delete('/:id', (req, res, next) => {
+  Blog.findByIdAndRemove(req.params.id).then(() => {
+    res.status(204).end();
+  });
+});
 
 blogsRouter.put('/:id', async (req, res, next) => {
-  const body = req.body
+  const body = req.body;
   const blog = {
     likes: body.likes,
-  }
+  };
 
-  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
-  res.json(updatedBlog)
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+    new: true,
+  });
+  res.json(updatedBlog);
+});
 
-})
-
-
-
-
-module.exports = blogsRouter
+module.exports = blogsRouter;
--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,32 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
-})
+  const users = await User.find({});
+  response.json(users);
+});
 
 usersRouter.post('/', async (request, response) => {
-  const body = request.body
+  const body = request.body;
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
   const user = new User({
     username: body.username,
     name: body.name,
     passwordHash,
-  })
+  });
 
-  const savedUser = await user.save()
+  const savedUser = await user.save();
 
-  response.json(savedUser)
-})
+  response.json(savedUser);
+});
 
-module.exports = usersRouter
+usersRouter.delete('/:id', async (req, res) => {
+  await User.findByIdAndRemove(req.params.id);
+  res.status(204).end();
+});
+
+module.exports = usersRouter;
